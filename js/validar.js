9b5a6e7a4703d4537f5e45015c41380d2e5ff410
Ricardo2277 <112729838+Ricardo2277@users.noreply.github.com>
2022-12-20 08:36:44 -0600

    Add files via upload

---
+++ js/validar.js
@@ -0,0 +1,7 @@
+const btnEnv=document.querySelector('.btn')
+btnEnv.addEventListener('click',(e)=>{
+    const checar1=document.getElementById('check1').Checked;
+    if(checar1){
+        alert('seleccionado')
+    }
+})
